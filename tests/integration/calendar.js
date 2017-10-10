fe72a8db28aa43c0ae4676c72e71bda18e622f24
Sam Richard <sam@snug.ug>
2017-10-10 16:39:30 -0500

    :white_check_mark: Add real basic integration tests

---
+++ tests/integration/calendar.js
@@ -0,0 +1,32 @@
+import test from 'ava';
+import puppeteer from 'puppeteer';
+import server from '../test-server';
+
+// Set up a new server for each test
+test.cb.beforeEach(t => {
+  t.context.port = ~~(Math.random() * 1000) + 8000; // eslint-disable-line no-param-reassign
+  t.context[`server-${t.context.port}`] = server.listen(t.context.port, 'localhost', () => { // eslint-disable-line no-param-reassign
+    t.end();
+  });
+});
+
+// Break down server after each test
+test.afterEach.cb(t => {
+  t.context[`server-${t.context.port}`].close();
+  t.end();
+});
+
+test('Today has focus', async t => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(`http://localhost:${t.context.port}`);
+  await page.press('Tab');
+
+  const active = await page.evaluate(() => {
+    return document.activeElement;
+  });
+
+  await browser.close();
+
+  t.is(active._prevClass, 'calendar__link');
+});
